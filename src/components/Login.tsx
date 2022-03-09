f1eaf679355cc98634035801e650ecdd015df74e
Stellarlay <Stellarlay@yandex.ru>
2022-03-09 21:20:26 +0300

    Start project commit

---
+++ src/components/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './img/loginBg.jpg';
+
+const Login = () => {
+    return (
+        <div>
+            <img src={logo} alt='login-bg' width='500px'/>
+        </div>
+    );
+}
+
+export default Login;
