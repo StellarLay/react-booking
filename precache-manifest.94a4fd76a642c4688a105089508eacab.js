e95617dfe56ff1cf666d20734e1f644e2f707af7
Stellarlay <Stellarlay@yandex.ru>
2022-03-22 23:49:00 +0300

    Updates

---
+++ precache-manifest.94a4fd76a642c4688a105089508eacab.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1b73e6d7af89b25720f",
+    "url": "/react-booking/static/css/main.eba11f7d.chunk.css"
+  },
+  {
+    "revision": "b1b73e6d7af89b25720f",
+    "url": "/react-booking/static/js/main.b1b73e6d.chunk.js"
+  },
+  {
+    "revision": "b28ca12bc3a9e68e8a17",
+    "url": "/react-booking/static/css/1.cf13553f.chunk.css"
+  },
+  {
+    "revision": "b28ca12bc3a9e68e8a17",
+    "url": "/react-booking/static/js/1.b28ca12b.chunk.js"
+  },
+  {
+    "revision": "c36587dca287557fa3ca",
+    "url": "/react-booking/static/js/runtime~main.c36587dc.js"
+  },
+  {
+    "revision": "9eecee601abbf628df636703dc0d19f3",
+    "url": "/react-booking/static/media/loginBg.9eecee60.jpg"
+  },
+  {
+    "revision": "d8be483152b0d6b0046229742066a2d2",
+    "url": "/react-booking/static/media/hotel_1.d8be4831.jpg"
+  },
+  {
+    "revision": "f8abef80779b66c52d74402ccb8b3361",
+    "url": "/react-booking/static/media/hotel_2.f8abef80.jpg"
+  },
+  {
+    "revision": "8cae5e98921b6a12e708b92d69d462e1",
+    "url": "/react-booking/static/media/hotel_3.8cae5e98.jpg"
+  },
+  {
+    "revision": "bb72906466ae8d4de3f710bf37e3c740",
+    "url": "/react-booking/static/media/hotel_4.bb729064.jpg"
+  },
+  {
+    "revision": "c30e7a7c4fd58370ed4e739be525fbfe",
+    "url": "/react-booking/static/media/hotel_5.c30e7a7c.jpg"
+  },
+  {
+    "revision": "cee508ce04af1768033c28f6022927bf",
+    "url": "/react-booking/static/media/map.cee508ce.png"
+  },
+  {
+    "revision": "4700b8b84638de7848fe0699c5023dfd",
+    "url": "/react-booking/index.html"
+  }
+];
