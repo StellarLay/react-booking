1e51feba13211ec0d38cda010e0d5cfe4315ccbc
Stellarlay <Stellarlay@yandex.ru>
2022-03-22 23:43:58 +0300

    Have prepared files for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Login from './components/Login';
 function App() {
   const [isLogin, setIsLogin] = useState(false);
 
-  if (!isLogin) {
+  if (isLogin) {
     return (
       <div className='homeApp'>
         <Home getUserStatus={setIsLogin} />
